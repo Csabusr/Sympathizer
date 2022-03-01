f5131108557187cee242559af881ddf9ea7a2a93
Fügedi Csaba <fugedic@gmail.com>
2022-03-01 09:07:43 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/Source/UI/Colours.h
+++ src/Source/UI/Colours.h
@@ -11,9 +11,9 @@
 #pragma once
 #include <JuceHeader.h>
 namespace OurColours {
-    const juce::Colour BaseColour{ 0xff132f40 };
-    const juce::Colour HighlightColor{ 0xffbfa3b2 };
-    const juce::Colour BaseColourBright{ 0xff3c5e73 };
-    const juce::Colour BaseColourBrighter{ 0xff456b8c };
-    const juce::Colour BaseColourBrightest{ 0xff7ea1bf };
+    const juce::Colour BaseColour{ 0xff010326 };
+    const juce::Colour HighlightColor{ 0xffF2E205 };
+    const juce::Colour BaseColourBright{ 0xffF2F2F2 };
+    const juce::Colour BaseColourBrighter{ 0xff8C8411 };
+    const juce::Colour BaseColourBrightest{ 0xff242426 };
 }
