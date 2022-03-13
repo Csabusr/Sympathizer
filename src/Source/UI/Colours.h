4e7d4303fe6896172be0e7d466c2461aa17e9ef0
Fügedi Csaba <fugedic@gmail.com>
2022-03-13 08:53:34 +0100

    Changed and added Colours

--- src/Source/UI/Colours.h
+++ src/Source/UI/Colours.h
@@ -11,9 +11,16 @@
 #pragma once
 #include <JuceHeader.h>
 namespace OurColours {
-    const juce::Colour BaseColour{ 0xff010326 };
-    const juce::Colour HighlightColor{ 0xffF2E205 };
+    const juce::Colour BaseColour{ 0xff214d43 };
+    const juce::Colour HighlightColor{ 0xfff9ebc5 };
     const juce::Colour BaseColourBright{ 0xffF2F2F2 };
-    const juce::Colour BaseColourBrighter{ 0xff8C8411 };
+    const juce::Colour BaseColourBrighter{ 0xff000000 };
     const juce::Colour BaseColourBrightest{ 0xff242426 };
+    const juce::Colour BaseTransparent{ 0x0000000 };
+
+    const juce::Colour ActiveRed{ 0xfff8460b };
+    const juce::Colour ActiveRedFirstRadius{ 0xff000000};
+    const juce::Colour ActiveRedOutline{ 0x22ab3109 };
+    const juce::Colour ScrewHeadBase{ 0xffb2b2b2 };
+    const juce::Colour ScrewHeadBase2{ 0xff000000 };
 }
